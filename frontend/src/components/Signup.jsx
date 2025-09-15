6402cfa7a6d94ba9e8010417a69b25045e534045
Anjali kumari choudhary <122722274+anjchoudhary@users.noreply.github.com>
2025-09-15 14:27:17 +0530

    Update Signup.jsx

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -22,7 +22,7 @@ function Signup() {
     email:data.email,
     password:data.password,
    }
-   await axios.post("http://localhost:4001/user/signup",userInfo).then((res)=>{
+   await axios.post("https://book-store-don6.onrender.com/user/signup",userInfo).then((res)=>{
     console.log(res.data)
     if(res.data){
       
@@ -143,3 +143,4 @@ function Signup() {
 }
 
 export default Signup;
+
