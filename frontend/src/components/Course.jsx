58010be2d0078fd825171cbd2f125d7ef2a048de
Anjali kumari choudhary <122722274+anjchoudhary@users.noreply.github.com>
2025-09-15 14:25:24 +0530

    Update Course.jsx

--- frontend/src/components/Course.jsx
+++ frontend/src/components/Course.jsx
@@ -10,7 +10,7 @@ function Course() {
   useEffect(() => {
 const getBook = async ()=>{
   try {
-   const res= await axios.get("http://localhost:4001/book");
+   const res= await axios.get("https://book-store-don6.onrender.com/book");
    console.log(res.data);
     setBook(res.data); 
   } catch (error) {
@@ -40,5 +40,6 @@ getBook();
  </>
   )
 }
+
 
 export default Course
