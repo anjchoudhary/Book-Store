58b6f90a297fb06989232f1db649eb6a8e75dc5c
Anjali kumari choudhary <122722274+anjchoudhary@users.noreply.github.com>
2025-09-15 14:26:40 +0530

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -17,7 +17,7 @@ function Login() {
     email:data.email,
     password:data.password,
    }
-   await axios.post("http://localhost:4001/user/login",userInfo).then((res)=>{
+   await axios.post("https://book-store-don6.onrender.com/user/login",userInfo).then((res)=>{
     console.log(res.data)
     if(res.data){
     
@@ -112,5 +112,6 @@ function Login() {
     </div>
   )
 }
+
 
 export default Login
